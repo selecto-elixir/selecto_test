52a29fc807320f1467a2ba21945630caa9379c38
Chris Rohlfs <seeken@gmail.com>
2022-10-19 11:56:06 -0600

    add agg

--- lib/listable_test_web/live/page_live.ex
+++ lib/listable_test_web/live/page_live.ex
@@ -17,11 +17,12 @@ defmodule ListableTestWeb.PageLive do
         show_view: false,
 
         ### required for lsitable components
-        view_sel: "detail",
+        view_mode: "detail",
         active_tab: "view",
         group_by: prep_sels(listable.set.group_by),
         order_by: prep_sels(listable.set.order_by),
         selected: prep_sels(listable.set.selected),
+        aggregate: [],
         listable: listable
       )
 
