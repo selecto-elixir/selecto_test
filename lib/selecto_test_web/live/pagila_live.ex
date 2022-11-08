dc0093c72d2efe3dedb1705d5b96bb957292eb1e
Chris Rohlfs <seeken@gmail.com>
2022-11-08 15:40:33 -0700

    view cleanups

--- lib/selecto_test_web/live/pagila_live.ex
+++ lib/selecto_test_web/live/pagila_live.ex
@@ -5,9 +5,14 @@ defmodule SelectoTestWeb.PagilaLive do
 
   use SelectoComponents.ViewSelector
 
+
+  ###
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    selecto = Selecto.configure(SelectoTest.Repo, selecto_domain())
+
+    {:ok, assign(socket, executed: false, show_view: false, selecto: selecto ) }
   end
 
   def film_link(row) do
@@ -32,6 +37,38 @@ defmodule SelectoTestWeb.PagilaLive do
 
   end
 
+
+
+  @impl true
+  def handle_params(_params, _uri, socket) do
+
+    socket =
+      assign(socket,
+        ### required for lsitable components
+        view_mode: "detail",
+        applied_view: "detail",
+        active_tab: "view",
+        aggregate: [],
+        group_by: [],
+        order_by: [],
+        selected: [],
+        filters: [],
+        per_page: 30,
+        page: 0,
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("toggle_show_view", _par, socket) do
+    {:noreply, assign(socket, show_view: !socket.assigns.show_view)}
+  end
+
+
+  @doc """
+  Test Domain
+  """
   defp selecto_domain() do
     %{
       source: SelectoTest.Store.Actor,
@@ -46,7 +83,6 @@ defmodule SelectoTestWeb.PagilaLive do
           process: &process_film_card/2
         }
       },
-
       joins: [
         film_actors: %{
           name: "Actor-Film Join",
@@ -60,9 +96,6 @@ defmodule SelectoTestWeb.PagilaLive do
                   format: :link,
                   link_parts: &film_link/1
                 },
-
-
-
               }
             }
           ]
@@ -71,40 +104,4 @@ defmodule SelectoTestWeb.PagilaLive do
     }
   end
 
-  @impl true
-  def handle_params(_params, _uri, socket) do
-    selecto = Selecto.configure(SelectoTest.Repo, selecto_domain())
-
-    socket =
-      assign(socket,
-        ### Changte to LiveView.JS TODO
-        show_view: false,
-
-        ### required for lsitable components
-        view_mode: "detail",
-        applied_view: "detail",
-        active_tab: "view",
-        group_by: prep_sels(selecto.set.group_by),
-        order_by: prep_sels(selecto.set.order_by),
-        selected: prep_sels(selecto.set.selected),
-        filters: [],
-        aggregate: [],
-        per_page: 30,
-        page: 0,
-        executed: false,
-        selecto: selecto
-      )
-
-    {:noreply, socket}
-  end
-
-  # handle this better. TODO
-  defp prep_sels(list) do
-    list |> Enum.map(fn item -> {UUID.uuid4(), item, %{}} end)
-  end
-
-  @impl true
-  def handle_event("toggle_show_view", _par, socket) do
-    {:noreply, assign(socket, show_view: !socket.assigns.show_view)}
-  end
 end
