f30954b9806bdcd03a118aa1463801651849d1d9
Chris Rohlfs <seeken@gmail.com>
2022-11-25 22:17:02 -0700

    group by filter select upd

--- lib/selecto_test/pagila_domain.ex
+++ lib/selecto_test/pagila_domain.ex
@@ -25,9 +25,9 @@ defmodule SelectoTest.PagilaDomain do
         "full_name" => %{
           name: "Full Name",
           select: {:concat, ["first_name", {:literal, " "}, "last_name"]},
-          #group_by_format: fn a -> a end,
+          group_by_format: fn a, _def -> a end,
           group_by_filter: "actor_id",
-          group_by_filter_select: "actor_id"
+          group_by_filter_select: ["full_name", "actor_id"]
         },
 
         #### Example Custom Column Component with subquery and config component
