31a3c079bd195a725894ff24f382050e567c4901
Chris Rohlfs <seeken@gmail.com>
2022-12-02 15:33:21 -0700

    refactoring

--- lib/selecto_test_web/live/pagila_live.ex
+++ lib/selecto_test_web/live/pagila_live.ex
@@ -10,7 +10,56 @@ defmodule SelectoTestWeb.PagilaLive do
   def mount(_params, _session, socket) do
     selecto = Selecto.configure(SelectoTest.Repo, SelectoTest.PagilaDomain.domain())
 
-    {:ok, assign(socket, selecto: selecto, show_view: false)}
+    {:ok,
+      assign(socket,
+        selecto: selecto,
+        show_view: false,
+
+        ###
+        executed: false,
+        applied_view: nil,
+
+        ### Build the view:
+        view_config: %{
+
+          view_mode: "aggregate",
+          active_tab: "view",
+          per_page: 30,
+          page: 0,
+          aggregate: Map.get(selecto.domain, :default_aggregate, []) |> set_defaults(),
+          group_by: Map.get(selecto.domain, :default_group_by, []) |> set_defaults(),
+          order_by: Map.get(selecto.domain, :default_order_by, []) |> set_defaults(),
+          selected: Map.get(selecto.domain, :default_selected, []) |> set_defaults(),
+          filters: []
+        }
+      )}
+  end
+
+
+  @impl true
+  def handle_params(_params, _uri, socket) do
+
+    # socket =
+    #   assign(socket,
+    #     ### required for selecto components
+
+    #     view_mode: params["view_mode"] || "detail",
+    #     active_tab: params["active_tab"] || "view",
+    #     per_page:
+    #       if params["per_page"] do
+    #         String.to_integer(params["per_page"])
+    #       else
+    #         30
+    #       end,
+    #     page:
+    #       if params["page"] do
+    #         String.to_integer(params["page"])
+    #       else
+    #         0
+    #       end
+    #   )
+
+    {:noreply, socket}
   end
 
   @impl true
