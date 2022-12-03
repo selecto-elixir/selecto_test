9c372b5edc86c539dd604aafe6d6299553d205ab
Chris Rohlfs <seeken@gmail.com>
2022-12-03 14:58:49 -0700

    dd my_path

--- lib/selecto_test_web/live/pagila_live.ex
+++ lib/selecto_test_web/live/pagila_live.ex
@@ -6,51 +6,17 @@ defmodule SelectoTestWeb.PagilaLive do
   use SelectoComponents.ViewSelector
   ###
 
-  def linker(t) do
-    IO.inspect(t)
-    ~p"/pagila"
-  end
-
-  @impl true
+   @impl true
   def mount(_params, _session, socket) do
     IO.inspect(socket)
     selecto = Selecto.configure(SelectoTest.Repo, SelectoTest.PagilaDomain.domain())
     state = get_initial_state(selecto)
 
-    socket = assign(socket, show_view_configurator: false )
+    socket = assign(socket,
+      show_view_configurator: false,
+      my_path: "/pagila"
+    )
     {:ok, assign(socket, state)}
-  end
-
-
-
-  @impl true
-  def handle_params(_params, _uri, socket) do
-
-    ### Handle page
-
-
-
-    # socket =
-    #   assign(socket,
-    #     ### required for selecto components
-
-    #     view_mode: params["view_mode"] || "detail",
-    #     active_tab: params["active_tab"] || "view",
-    #     per_page:
-    #       if params["per_page"] do
-    #         String.to_integer(params["per_page"])
-    #       else
-    #         30
-    #       end,
-    #     page:
-    #       if params["page"] do
-    #         String.to_integer(params["page"])
-    #       else
-    #         0
-    #       end
-    #   )
-
-    {:noreply, socket}
   end
 
   @impl true
