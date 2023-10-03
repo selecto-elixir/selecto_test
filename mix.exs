1083f72bfe280a03ce02ea79a734de0b0722c164
Chris Rohlfs <seeken@gmail.com>
2023-10-02 18:34:51 -0600

    Need this for fly to build :/

--- mix.exs
+++ mix.exs
@@ -36,6 +36,8 @@ defmodule SelectoTest.MixProject do
       {:phoenix_ecto, "~> 4.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
+      {:ecto_sql, "~> 3.10"},
+
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.20", override: true},
       {:heroicons, "~> 0.5"},
