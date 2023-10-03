45721b6dc38b668e0960db5c8d93478d8fe69553
Chris Rohlfs <seeken@gmail.com>
2023-10-02 18:19:03 -0600

    Ecto 3.10

--- mix.exs
+++ mix.exs
@@ -34,7 +34,6 @@ defmodule SelectoTest.MixProject do
     [
       {:phoenix, "~> 1.7.7", override: true},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
