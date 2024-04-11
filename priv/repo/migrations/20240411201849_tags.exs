5204fc8c8e174b44df56799b1198377260f6ba45
Chris Rohlfs <seeken@gmail.com>
2024-04-11 14:25:01 -0600

    Add tags migration

---
+++ priv/repo/migrations/20240411201849_tags.exs
@@ -0,0 +1,28 @@
+defmodule SelectoTest.Repo.Migrations.Tags do
+  use Ecto.Migration
+
+  def change do
+    create table(:tag) do
+      add :name, :string
+
+      timestamps()
+    end
+
+    create table(:film_tag) do
+      add :film_id, references(:film, on_delete: :delete_all, column: :film_id)
+      add :tag_id, references(:tag, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+
+    create(
+      unique_index(
+      :tag,
+        ~w(name)a,
+        name: :index_for_tag_name
+      )
+    )
+
+  end
+end
