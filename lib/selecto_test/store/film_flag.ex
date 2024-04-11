21b6a2f6ea998bda414fbbd60b67aac738f7b9ec
Chris Rohlfs <seeken@gmail.com>
2024-04-11 14:48:02 -0600

    flags code & seeds

---
+++ lib/selecto_test/store/film_flag.ex
@@ -0,0 +1,17 @@
+defmodule SelectoTest.Store.FilmFlag do
+  use Ecto.Schema
+
+  @primary_key false
+
+  schema "film_flag" do
+    # has_one flag
+    belongs_to :flag, SelectoTest.Store.Flag, primary_key: true
+    # has_one film
+    belongs_to :film, SelectoTest.Store.Film, primary_key: true, references: :film_id
+
+    field :value, :string
+    timestamps()
+
+
+  end
+end
---
+++ lib/selecto_test/store/tag.ex
@@ -0,0 +1,10 @@
+defmodule SelectoTest.Store.Tag do
+  use Ecto.Schema
+
+  schema "tag" do
+    field :name, :string
+
+    timestamps()
+
+  end
+end
