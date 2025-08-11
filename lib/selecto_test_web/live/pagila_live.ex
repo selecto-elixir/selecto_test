3b7bf606808299d201e5b29bdde890110eb787f4
Chris Rohlfs <seeken@gmail.com>
2025-08-10 22:54:00 -0600

    Enhance Postgrex connection handling in PagilaLive and PageControllerTest setup

--- lib/selecto_test_web/live/pagila_live.ex
+++ lib/selecto_test_web/live/pagila_live.ex
@@ -13,7 +13,20 @@ defmodule SelectoTestWeb.PagilaLive do
         :films -> {SelectoTest.PagilaDomainFilms, SelectoTest.PagilaDomainFilms.domain(), "/pagila_films"}
       end
 
-    selecto = Selecto.configure(domain, SelectoTest.Repo)
+    # Get Postgrex connection options from Repo config and start connection
+    repo_config = SelectoTest.Repo.config()
+    postgrex_opts = [
+      username: repo_config[:username],
+      password: repo_config[:password],
+      hostname: repo_config[:hostname], 
+      database: repo_config[:database],
+      port: repo_config[:port] || 5432
+    ]
+    
+    # Start a Postgrex connection process for Selecto to use
+    {:ok, db_conn} = Postgrex.start_link(postgrex_opts)
+    
+    selecto = Selecto.configure(domain, db_conn)
 
     views = [
       {:aggregate, SelectoComponents.Views.Aggregate, "Aggregate View", %{drill_down: :detail}},
