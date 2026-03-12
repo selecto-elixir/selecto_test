7c178e8c27129020a1b9b220266f40a63d562c12
Chris Rohlfs <seeken@gmail.com>
2026-03-11 21:09:42 -0600

    fix: use colocated SelectoComponents hooks

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,7 +1,5 @@
 import ColorSchemeHook from "./color-scheme-hook";
-import selectoComponentsHooks from "./selecto-components-hooks";
 
 export default {
-  ColorSchemeHook,
-  ...selectoComponentsHooks
+  ColorSchemeHook
 };
--- assets/js/selecto_components_hooks.js
+++
@@ -1,4 +0,0 @@
-// Use Phoenix LiveView's generated colocated hook bundle for selecto_components.
-import { hooks as colocatedSelectoComponentsHooks } from "phoenix-colocated/selecto_components";
-
-export const selectoComponentsHooks = colocatedSelectoComponentsHooks || {};
